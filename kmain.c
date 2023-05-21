734325ec8179fd2cebb2a554b897de93fd93d6ef
ghghghg2 <sam7793085@gmail.com>
2023-05-22 00:23:25 +0800

    Test serial driver

--- kmain.c
+++ kmain.c
@@ -1,20 +1,20 @@
 #include "io.h"
 #include "display.h"
+#include "serial.h"
 
 int kmain(void)
 {
-    char str[] = "Hello, this is a demo message1\n Hello, this is a demo message*\n";
-    // char ch = 'A';
-    // write(str, sizeof(str)-1);
-    for (int i = 0; i < 10; i++) {
-        write(str, sizeof(str)-1);
-    }
+    char str[] = "Hello Serial.\n";
+    char str2[] = "End Serial.\n";
 
-    // for (int i = 0; i < 2005; i++) {
-    //     // ch = 'A' + ((i / 80) % 26);
-    //     ch = 'A' + (i % 26);
-    //     write(&ch, 1);
-    // }
+    serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
+    serial_configure_line(SERIAL_COM1_BASE);
+    serial_configure_fifo(SERIAL_COM1_BASE);
+    serial_configure_modem(SERIAL_COM1_BASE);
+
+    write(str, sizeof(str)-1);
+    serial_write(SERIAL_COM1_BASE, str, sizeof(str)-1);
+    write(str2, sizeof(str2)-1);
 
     return 0xdeafbeef;
 }
