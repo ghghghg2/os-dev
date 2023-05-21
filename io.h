dfc28bf04c57a61ed6448cccb3c7358265a0e5c0
ghghghg2 <sam7793085@gmail.com>
2023-05-22 00:21:38 +0800

    Implement inb in io.s

--- io.h
+++ io.h
@@ -10,5 +10,13 @@
  */
 void __attribute__((cdecl)) outb(unsigned short port, unsigned char data);
 
+/** inb:
+ *  Read a byte from an I/O port.
+ *
+ *  @param  port The address of the I/O port
+ *  @return      The read byte
+ */
+unsigned char __attribute__((cdecl)) inb(unsigned short port);
+
 
 #endif
