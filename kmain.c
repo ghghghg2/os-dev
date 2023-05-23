bb4a069e381249e6893073d6b70d2ea603b6a16d
ghghghg2 <sam7793085@gmail.com>
2023-05-23 23:30:46 +0800

    Add gdt fcn to proj

--- kmain.c
+++ kmain.c
@@ -1,11 +1,15 @@
 #include "io.h"
 #include "display.h"
 #include "serial.h"
+#include "gdt.h"
 
 int kmain(void)
 {
     char str[] = "Hello Serial.\n";
     char str2[] = "End Serial.\n";
+    char str3[] = "Serial Log\n";
+
+    gdt_initialize();
 
     serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
     serial_configure_line(SERIAL_COM1_BASE);
@@ -13,8 +17,8 @@ int kmain(void)
     serial_configure_modem(SERIAL_COM1_BASE);
 
     write(str, sizeof(str)-1);
-    serial_write(SERIAL_COM1_BASE, str, sizeof(str)-1);
+    serial_write(SERIAL_COM1_BASE, str3, sizeof(str3)-1);
     write(str2, sizeof(str2)-1);
 
-    return 0xdeafbeef;
+    return 0xdeadbeef;
 }
